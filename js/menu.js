53af2837884beb57bf2cfd44eea6c957976a93ce
nahuel rodridguez <nahuel25-programacion@gmail.com>
2026-08-24 17:40:04 -0300

    Primer commit

---
+++ js/menu.js
@@ -0,0 +1,12 @@
+const menuToggle = document.getElementById('menu-toggle');
+
+const menu = document.getElementById('menu');
+
+
+menuToggle.addEventListener('click', () => {
+
+
+    menu.classList.toggle('active');
+
+
+});
